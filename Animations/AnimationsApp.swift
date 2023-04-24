67eb6eede7143475263ad0d53d5d1b0ef314ba10
Aficer <Aficer89@gmail.com>
2023-04-24 16:51:34 +0300

    animation
    First commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Вадим Эфис on 24.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
